d8221199569c3c5372bd19d7f1c59cb42cf53415
Claire Lee <lee.claire156@gmail.com>
2023-06-20 20:59:37 -0400

    Adds marklar and even_fibonacci functions

--- exercise.rb
+++ exercise.rb
@@ -4,7 +4,50 @@ class Exercise
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
-    # TODO: Implement this method
+  # splits string by spaces
+    array = str.split(" ")
+  
+    marklarizedArray = []
+    tempArray = []
+
+  # returns word back if its less 4 character or less, returns appropriate M/marklar for others
+    array.map do |x|
+      if x.length < 5 
+        marklarizedArray.push(x)
+      elsif x =~ /\p{P}/
+        tempArray = x.split(/\p{P}/), x.scan(/\p{P}/)
+        tempArray2 = tempArray.flatten
+        if tempArray2[0] =~ /[A-Z]/
+          tempArray2[0] = "Marklar"
+        else
+          tempArray2[0] = "marklar"
+        end
+        tempArray2 = tempArray2.join
+        marklarizedArray.push(tempArray2)
+      elsif x =~ /[A-Z]/
+        marklarizedArray.push("Marklar")
+      else
+        marklarizedArray.push("marklar")
+      end
+    end
+
+  # Puts string back together
+    marklarizedString = []
+    i = 0
+    while i < marklarizedArray.length
+      if i == 0
+        marklarizedString.push("#{marklarizedArray[i]} ")
+      elsif i == marklarizedArray.length - 1
+        marklarizedString.push("#{marklarizedArray[i]}")
+      else
+        marklarizedString.push("#{marklarizedArray[i]} ")
+      end
+      i += 1
+    end
+    
+    marklarizedString = marklarizedString.join("")
+    
+    return marklarizedString
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
@@ -12,7 +55,25 @@ class Exercise
   # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
-    # TODO: Implement this method
-  end
+    #fibonacci sequence
+    array = [1, 1]
+    i = 2
+    while i < nth
+      a = array[i - 1]
+      b = array[i - 2]
+      array.push(a+b)
+      i+=1 
+    end
 
+    #takes out only even int in fibonaccci sequence
+    arrayEvensOnly = []
+    j = 0
+    array.map do |x|
+      if x.remainder(2) == 0
+        arrayEvensOnly.push(x)        
+      end
+    end
+    
+    return arrayEvensOnly.sum
+  end
 end
